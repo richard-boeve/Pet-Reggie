b7c2291437a31f921d04121d5275309a4a4a6d35
student <richardboeve@hotmail.com>
2018-08-30 11:20:39 +1200

    react up and running

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,12 @@
-const express = require('express')
+// const express = require('express')
 
-//const db = require('../db')
+// //const db = require('../db')
 
-const router = express.Router()
+// const router = express.Router()
 
-router.get('/', (req, res) => {
-   res.send('hello world')
-  })
+// router.get('/', (req, res) => {
+//    res.send('hello world')
+//   })
 
 
-module.exports = router
+// module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const path = require('path')
 const express = require('express')
 const server = express()
-const routes = require('./routes/route')
+const bodyParser = require('body-parser')
+//onst routes = require('./routes/route')
 
-server.use('/', routes)
+//server.use('/', routes)
+
+server.use(bodyParser.json())
+server.use(express.static(path.join(__dirname, 'public')))
 
 
 module.exports = server
