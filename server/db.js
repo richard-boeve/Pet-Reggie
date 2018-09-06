3e99e51cf2ab07c80da144c1eaa8559297a0db87
student <richardboeve@hotmail.com>
2018-09-06 17:24:35 +1200

    working on routes

--- server/db.js
+++ server/db.js
@@ -2,12 +2,14 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('../knexfile')[environment]
 const connection = require('knex')(config)
 
-module.exports = {
-    getOwners: getOwners
-}
+
 
 function getOwners (testConn) {
     const conn = testConn || connection
     return conn ('owners')
     .select()
+}
+
+module.exports = {
+    getOwners: getOwners
 }
--- server/routes.js
+++ server/routes.js
@@ -3,13 +3,17 @@ const router = express.Router()
 
 const db = require('./db')
 
-// router.get('/', function(req,res){
-//     res.send('hello')
-// })
 
 router.get('/owners', function(req, res){
-    const owners = db.getOwners()
-    res.json(owners)
+    db.getOwners(req.params)
+    .then(owners => {
+        res.json(owners)
+    })
 })
 
 module.exports = router
+
+// router.get('/owners', function(req, res){
+//     const owners = db.getOwners()
+//     res.json(owners)
+// })
